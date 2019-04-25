dc06b108c4d6758ca37e1d12d24783bc3e3213ea
Ian Carlos <carlos.ian007@gmail.com>
2019-04-25 16:24:54 -0300

    Feat(database): Add Image Upload

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const routes = express.Router()
+const multerConfig = require('../src/config/multer')
+const upload = require('multer')(multerConfig)
 
 const UserController = require('./app/controllers/UserController')
 
 routes.get('/signup', UserController.create)
-routes.post('/signup', UserController.store)
+routes.post('/signup', upload.single('avatar'), UserController.store)
 
 module.exports = routes
