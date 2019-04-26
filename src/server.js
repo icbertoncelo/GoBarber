31c54c103cfd6399c7a4596833ead26b220a01a9
Ian Carlos <carlos.ian007@gmail.com>
2019-04-25 23:13:50 -0300

    Feat(login): Add User Session

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const session = require('express-session')
+const LokiStore = require('connect-loki')(session)
 const path = require('path')
 
 class App {
@@ -14,6 +16,16 @@ class App {
 
   middlewares () {
     this.express.use(express.urlencoded({ extended: false }))
+    this.express.use(
+      session({
+        store: new LokiStore({
+          path: path.resolve(__dirname, '..', 'tmp', 'sessions.db')
+        }),
+        secret: 'MySecretApp',
+        resave: true,
+        saveUninitialized: true
+      })
+    )
   }
 
   views () {
